c3798ccf1fdd9cbe0d0606a2505d1970b6f89217
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-01-19 14:11:35 -0300

    Project created

---
+++ backend/src/test/java/io/celeiro/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.celeiro.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
