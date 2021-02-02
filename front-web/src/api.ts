e013355403b7e4afec6035bc2b5e3335e576cc1c
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-02-01 23:25:06 -0300

    Add OrderLocation and Mapbox

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-const API_URL = 'https://geuel-sds2.herokuapp.com'
+const API_URL = 'https://geuel-sds2.herokuapp.com';
+const mapboxToken = '';
 
 export function fetchProducts() {
-  return axios(`${API_URL}/products`)
+  return axios(`${API_URL}/products`);
+}
+
+export function fetchLocalMapBox(local: string) {
+  return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
