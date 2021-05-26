0e110d2088e813e3ecc0e3722a349243d0d73551
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-05-25 22:01:20 -0300

    Ambient variable

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = 'https://geuel-sds2.herokuapp.com';
-const mapboxToken = '';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
   return axios(`${API_URL}/products`);
